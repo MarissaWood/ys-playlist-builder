26508e5cfb713d4cf8fb8c059bf0ea2e1cb7adec
Marissa Wood <marissa.espina@gmail.com>
2018-08-01 09:59:22 -0400

    items adding to playlist without data

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -11,7 +11,7 @@ class Playlist extends Component {
   }
 
   componentDidUpdate() {
-    if (this.state.playlist != this.props.list) {
+    if (this.state.playlist !== this.props.list) {
       this.setState({
         playlist: this.props.list
       });
